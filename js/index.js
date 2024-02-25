23c0245b4e24c9431c1be8b4da2351a335957dcd
larevolucia <reis.lucia@gmail.com>
2024-02-25 15:42:52 +0100

    time in selected location function

--- js/index.js
+++ js/index.js
@@ -1,56 +1,29 @@
-//function updateTime() {
-//   //    Rio de Janeiro
-
-//   let rioDeJaneiroElement = document.querySelector("#rio-de-janeiro");
-//   let rioDeJaneiroDateElement = rioDeJaneiroElement.querySelector(".date");
-//   let rioDeJaneiroTimeElement = rioDeJaneiroElement.querySelector(".time");
-//   let rioDeJaneiroTime = moment().tz("Brazil/East");
-//   rioDeJaneiroDateElement.innerHTML = `${rioDeJaneiroTime.format(
-//     "MMMM Do YYYY"
-//   )}`;
-//   rioDeJaneiroTimeElement.innerHTML = `${rioDeJaneiroTime.format(
-//     "h:mm:ss [<small>]A[</small>]"
-//   )}`;
-
-//   //    Amsterdam
-
-//   let amsterdamElement = document.querySelector("#amsterdam");
-//   let amsterdamDateElement = amsterdamElement.querySelector(".date");
-//   let amsterdamTimeElement = amsterdamElement.querySelector(".time");
-//   let amsterdamTime = moment().tz("Europe/Amsterdam");
-//   amsterdamDateElement.innerHTML = `${amsterdamTime.format("MMMM Do YYYY")}`;
-//   amsterdamTimeElement.innerHTML = `${amsterdamTime.format(
-//     "h:mm:ss [<small>]A[</small>]"
-//   )}`;
-
-//   //    Tokyo
-
-//   let tokyoElement = document.querySelector("#tokyo");
-//   let tokyoDateElement = tokyoElement.querySelector(".date");
-//   let tokyoTimeElement = tokyoElement.querySelector(".time");
-//   let tokyoTime = moment().tz("Asia/Tokyo");
-//   tokyoDateElement.innerHTML = `${tokyoTime.format("MMMM Do YYYY")}`;
-//   tokyoTimeElement.innerHTML = `${tokyoTime.format(
-//     "h:mm:ss [<small>]A[</small>]"
-//   )}`;
-// }
-// updateTime();
-// setInterval(updateTime, 1000);
-
 function showTime(event) {
   let cityElement = document.querySelector("#city-data");
   let cityDateElement = cityElement.querySelector(".date");
   let cityTimeElement = cityElement.querySelector(".time");
-  let cityTime = moment().tz(event.target.value);
+
+  const selectElement = document.getElementById("city-list");
+  const cityDiv = document.querySelector(".city-name");
+  const selectedOption = selectElement.options[selectElement.selectedIndex];
+  const selectedOptionHtml = selectedOption.innerHTML;
 
   if (event.target.value.length > 0) {
+    let cityTime = moment().tz(event.target.value);
+
     cityDateElement.innerHTML = `${cityTime.format("MMMM Do YYYY")}`;
     cityTimeElement.innerHTML = `${cityTime.format(
       "h:mm:ss [<small>]A[</small>]"
     )}`;
-    document.getElementById("city-name").textContent = selectedText;
+
+    cityDiv.innerHTML = selectedOptionHtml;
   }
 }
 
 let cityList = document.getElementById("city-list");
 cityList.addEventListener("change", showTime);
+
+setInterval(function () {
+  const event = { target: document.getElementById("city-list") };
+  showTime(event);
+}, 1000);
